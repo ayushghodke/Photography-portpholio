46c6745bf865830a77b570c64e99ad8a33c10630
ayushghodke <ayushghodke21@gmail.com>
2026-02-17 13:49:39 +0530

    one image removed

--- src/data/images.js
+++ src/data/images.js
@@ -1,170 +1,166 @@
 export const galleryImages = [
-  {
-    "src": "20241012_181024.webp",
-    "category": "General"
-  },
-  {
-    "src": "20241012_181530.webp",
-    "category": "General"
-  },
-  {
-    "src": "20241118_185456.webp",
-    "category": "General"
-  },
-  {
-    "src": "20241125_163515.webp",
-    "category": "General"
-  },
-  {
-    "src": "20241125_172320.webp",
-    "category": "General"
-  },
-  {
-    "src": "20241204_204205.webp",
-    "category": "General"
-  },
-  {
-    "src": "20241204_234823.webp",
-    "category": "General"
-  },
-  {
-    "src": "20241208_215136.webp",
-    "category": "General"
-  },
-  {
-    "src": "20250104_084736.webp",
-    "category": "General"
-  },
-  {
-    "src": "20250105_085829.webp",
-    "category": "General"
-  },
-  {
-    "src": "20250126_075739(0).webp",
-    "category": "General"
-  },
-  {
-    "src": "20250303_082637.webp",
-    "category": "General"
-  },
-  {
-    "src": "20250303_084946.webp",
-    "category": "General"
-  },
-  {
-    "src": "20250303_085546.webp",
-    "category": "General"
-  },
-  {
-    "src": "20250303_091753.webp",
-    "category": "General"
-  },
-  {
-    "src": "20251004_075919.webp",
-    "category": "General"
-  },
-  {
-    "src": "20251114_223910(1).webp",
-    "category": "General"
-  },
-  {
-    "src": "20260107_174107.webp",
-    "category": "General"
-  },
-  {
-    "src": "20260112_153827.webp",
-    "category": "General"
-  },
-  {
-    "src": "20260125_144432.webp",
-    "category": "General"
-  },
-  {
-    "src": "20260125_164810.webp",
-    "category": "General"
-  },
-  {
-    "src": "Portrait/20241125_164943.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20241125_171949.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20241205_092605.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20241208_193802.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20241208_194126.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20250104_083542.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20250105_102635.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20250909_154635.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20251001_111226.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20251004_075939.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20251226_160436.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20260107_174715.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20260112_100822.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20260209_143442.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20260209_203424.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Portrait/20260210_135039.webp",
-    "category": "Portrait"
-  },
-  {
-    "src": "Still Captures/20241012_195014.webp",
-    "category": "Still Captures"
-  },
-  {
-    "src": "Still Captures/20250105_094423.webp",
-    "category": "Still Captures"
-  },
-  {
-    "src": "Still Captures/20250330_115029.webp",
-    "category": "Still Captures"
-  },
-  {
-    "src": "Still Captures/20251004_072227.webp",
-    "category": "Still Captures"
-  },
-  {
-    "src": "Still Captures/20260110_093606.webp",
-    "category": "Still Captures"
-  }
+    {
+        "src": "20241012_181024.webp",
+        "category": "General"
+    },
+    {
+        "src": "20241012_181530.webp",
+        "category": "General"
+    },
+    {
+        "src": "20241118_185456.webp",
+        "category": "General"
+    },
+    {
+        "src": "20241125_163515.webp",
+        "category": "General"
+    },
+    {
+        "src": "20241125_172320.webp",
+        "category": "General"
+    },
+    {
+        "src": "20241204_204205.webp",
+        "category": "General"
+    },
+    {
+        "src": "20241204_234823.webp",
+        "category": "General"
+    },
+    {
+        "src": "20241208_215136.webp",
+        "category": "General"
+    },
+    {
+        "src": "20250104_084736.webp",
+        "category": "General"
+    },
+    {
+        "src": "20250105_085829.webp",
+        "category": "General"
+    },
+    {
+        "src": "20250126_075739(0).webp",
+        "category": "General"
+    },
+    {
+        "src": "20250303_082637.webp",
+        "category": "General"
+    },
+    {
+        "src": "20250303_084946.webp",
+        "category": "General"
+    },
+    {
+        "src": "20250303_085546.webp",
+        "category": "General"
+    },
+    {
+        "src": "20250303_091753.webp",
+        "category": "General"
+    },
+    {
+        "src": "20251004_075919.webp",
+        "category": "General"
+    },
+    {
+        "src": "20251114_223910(1).webp",
+        "category": "General"
+    },
+    {
+        "src": "20260107_174107.webp",
+        "category": "General"
+    },
+    {
+        "src": "20260112_153827.webp",
+        "category": "General"
+    },
+    {
+        "src": "20260125_144432.webp",
+        "category": "General"
+    },
+    {
+        "src": "20260125_164810.webp",
+        "category": "General"
+    },
+    {
+        "src": "Portrait/20241125_164943.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20241125_171949.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20241205_092605.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20241208_193802.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20241208_194126.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20250104_083542.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20250105_102635.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20250909_154635.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20251001_111226.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20251004_075939.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20260107_174715.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20260112_100822.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20260209_143442.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20260209_203424.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Portrait/20260210_135039.webp",
+        "category": "Portrait"
+    },
+    {
+        "src": "Still Captures/20241012_195014.webp",
+        "category": "Still Captures"
+    },
+    {
+        "src": "Still Captures/20250105_094423.webp",
+        "category": "Still Captures"
+    },
+    {
+        "src": "Still Captures/20250330_115029.webp",
+        "category": "Still Captures"
+    },
+    {
+        "src": "Still Captures/20251004_072227.webp",
+        "category": "Still Captures"
+    },
+    {
+        "src": "Still Captures/20260110_093606.webp",
+        "category": "Still Captures"
+    }
 ];
